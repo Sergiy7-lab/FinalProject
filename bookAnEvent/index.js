f7df46c73555f71459958526b4789be37e607819
Sergiy7-lab <67895331+Sergiy7-lab@users.noreply.github.com>
2020-10-19 15:46:12 +0300

    Update index.js

--- bookAnEvent/index.js
+++ bookAnEvent/index.js
@@ -2,6 +2,39 @@ let bg = document.querySelector(".block-paralax__img");
 
 window.addEventListener("scroll", function () {
   let value = window.scrollY;
-  bg.style.transform = `translateY(${(value - 1218) / 2}px)`;
-  console.log(value);
+  bg.style.transform = `translateY(${(value - 430) / 2}px)`;
+ 
 });
+
+if (window.matchMedia("(max-width: 1680px)").matches) {
+  window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+    bg.style.transform = `translateY(${(value - 642) / 2}px)`;
+    
+  });
+}
+
+if (window.matchMedia("(max-width: 1440px)").matches) {
+  window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+    bg.style.transform = `translateY(${(value - 830) / 2}px)`;
+    
+  });
+}
+
+
+if (window.matchMedia("(max-width: 1024px)").matches) {
+  window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+    bg.style.transform = `translateY(${(value - 650) / 2}px)`;
+    
+  });
+}
+
+if (window.matchMedia("(max-width: 425px)").matches) {
+  window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+    bg.style.transform = `translateY(${(value - 700) / 2}px)`;
+    
+  });
+}
