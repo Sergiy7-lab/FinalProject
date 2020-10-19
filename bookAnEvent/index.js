ababb724d0f274ffab1ff3935e084582ee5aa83e
Sergiy7-lab <67895331+Sergiy7-lab@users.noreply.github.com>
2020-10-19 16:10:19 +0300

    Add files via upload

--- bookAnEvent/index.js
+++ bookAnEvent/index.js
@@ -31,10 +31,18 @@ if (window.matchMedia("(max-width: 1024px)").matches) {
   });
 }
 
+if (window.matchMedia("(max-width: 650px)").matches) {
+  window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+    bg.style.transform = `translateY(${(value - 1000) / 3}px)`;
+    console.log(value);
+  });
+}
+
 if (window.matchMedia("(max-width: 425px)").matches) {
   window.addEventListener("scroll", function () {
     let value = window.scrollY;
-    bg.style.transform = `translateY(${(value - 700) / 2}px)`;
-    
+    bg.style.transform = `translateY(${(value - 1100) / 4}px)`;
+    // console.log(value);
   });
 }
